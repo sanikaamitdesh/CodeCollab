f1ba82c50fdac1b81f1fd22f03c67916521e2417
sanikaamitdesh <143930587+sanikaamitdesh@users.noreply.github.com>
2025-05-10 10:45:02 +0530

    Ready for Netlify deployment

---
+++ next.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: { unoptimized: true }
+  // Do NOT include output: 'export' for dynamic routes
+};
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    
-    images: { unoptimized: true }, // if using <Image> from next/image
-  };
-  
-  export default nextConfig;
-  
